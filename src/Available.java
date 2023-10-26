bf5486b880089bd44f9c75c965da0c019a169f4d
bjli0001 <bjli0001@stud.kea.dk>
2023-10-26 23:00:08 +0200

    Lavet nogle sikringer mod ugyldige indtastninger

--- src/Available.java
+++ src/Available.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Available {
     static int daynr;
     static String[] day = new String[17];
     static String dayName;
+    static String[] dayOptions = new String[5];
+    static ArrayList<String> timeOptions;
 
     static void available(String date){
 
@@ -12,6 +15,7 @@ public class Available {
 
         for (int j=0; j<5; j++) {
             day =Main.calender.get(daynr);
+            dayOptions[j]=date.split("/")[2];
             dayName=day[17];
 
             System.out.println(dayName+", "+date + " er følgende tider ledige: ");
@@ -55,12 +59,13 @@ public class Available {
                 }
 
             }
-            System.out.println("\n");
+            System.out.println();
             daynr++;
             date=Main.dates.get(daynr);
-
+            System.out.println();
         }
-        System.out.println("\n");
+
+
     }
     static void reserved(String date){
 
@@ -114,10 +119,60 @@ public class Available {
 
 
         }
-        System.out.println("\n");
+        System.out.println();
+
     }
 
+    static void timeCheck(String date){
+        timeOptions = new ArrayList<String>();
 
+        daynr=Main.dates.indexOf(date);
+
+        day =Main.calender.get(daynr);
+
+        for (int i = 1; i < 17; i++) {
+            if (day[i].equals("0")) {
+                switch (i) {
+                    case 1 -> timeOptions.add("10:00");
+
+                    case 2 -> timeOptions.add("10:30");
+
+                    case 3 -> timeOptions.add("11:00");
+
+                    case 4 -> timeOptions.add("11:30");
+
+                    case 5 -> timeOptions.add("12:00");
+
+                    case 6 -> timeOptions.add("12:30");
+
+                    case 7 -> timeOptions.add("13:00");
+
+                    case 8 -> timeOptions.add("13:30");
+
+                    case 9 -> timeOptions.add("14:00");
+
+                    case 10 -> timeOptions.add("14:30");
+
+                    case 11 -> timeOptions.add("15:00");
+
+                    case 12 -> timeOptions.add("15:30");
+
+                    case 13 -> timeOptions.add("16:00");
+
+                    case 14 -> timeOptions.add("16:30");
+
+                    case 15 -> timeOptions.add("17:00");
+
+                    case 16 -> timeOptions.add("17:30");
+                }
+
+
+            }
+
+        }
+
+
+    }
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -54,7 +54,8 @@ public class Main {
 
 
         while (true) {
-            String[] items = new String[]{"Se ledige tider", "Se/Ændre kunders tider", "Slet tid","Opret kunde", "Salg"};
+            System.out.println();
+            String[] items = new String[]{"Se ledige tider", "Se/Ændre kunders tider", "Se/slet tid på dato","Opret kunde", "Salg"};
             Menu.menu(items);
 
             switch (Menu.op) {
@@ -62,31 +63,44 @@ public class Main {
                     System.out.println("Indtast dato (yyyy/MM/dd):");
                     date = input.nextLine();
 
+                    if (dates.contains(date)){
+
                     Available.available(date);
                     System.out.println("Vil du booke tid?");
                     Menu.menu(new String[]{"Ja", "Nej"});
                     if (Menu.op == 1) {
                         System.out.println("Indtast dag (dd):");
                         String dayStr = input.nextLine();
-                        String[] dateArr = date.split("/");
-                        if (Integer.parseInt(dayStr)<Integer.parseInt(dateArr[2])){
-                            dateArr[1]=Integer.toString(Integer.parseInt(dateArr[1])+1);
+                        if (Arrays.asList(Available.dayOptions).contains(dayStr)) {
+                            String[] dateArr = date.split("/");
+                            if (Integer.parseInt(dayStr) < Integer.parseInt(dateArr[2])) {
+                                dateArr[1] = Integer.toString(Integer.parseInt(dateArr[1]) + 1);
+                            }
+                            dateArr[2] = dayStr;
+
+                            date = String.join("/", dateArr);
+                            Available.timeCheck(date);
+                            System.out.println("Indtast tidspunkt (tt:mm):");
+                            String timeStr = input.nextLine();
+
+                            if (Available.timeOptions.contains(timeStr)) {
+
+                                System.out.println("Telefon nr:");
+                                int teleNr = input.nextInt();
+                                input.nextLine();
+                                if (teleNr > 9999999 && teleNr < 100000000) {
+                                    String teleStr = Integer.toString(teleNr);
+                                    Book.book(date, timeStr, teleStr);
+                                } else System.out.println("Ugyldigt telefon nr.");
+                            } else {
+                                System.out.println("Ugyldigt tidspunkt valgt");
+                            }
+                        } else {
+                            System.out.println("Ugyldig dag valgt");
                         }
-                        dateArr[2] = dayStr;
 
-                        date = String.join("/", dateArr);
-
-                        System.out.println("Indtast tidspunkt (tt:mm):");
-                        String timeStr = input.nextLine();
-
-                        System.out.println("Telefon nr:");
-                        int teleNr = input.nextInt();
-                        input.nextLine();
-                        if (teleNr > 9999999 && teleNr < 100000000) {
-                            String teleStr = Integer.toString(teleNr);
-                            Book.book(date, timeStr, teleStr);
-                        } else System.out.println("Ugyldigt telefon nr.");
-
+                        }
+                    else System.out.println("Ugyldig dato valgt");
                     }
 
                 }
