e13980538fc0b6dcf2f6f4062faf88f85c247d83
Askethemervin <aske.johanson@gmail.com>
2023-10-27 14:58:02 +0200

    returns til passwords

--- src/Password.java
+++ src/Password.java
@@ -1,31 +1,36 @@
 import java.util.Scanner;
 
 public class Password {
+    static String passwords;
+    static String a;
 
 
-    public static void main(String[] args) {
-            Scanner input=new Scanner(System.in);
-            String password="hairyharry";
-            String a="";
-
-            System.out.println("Indtast dit Password");
-            System.out.println("Tast q for at afslutte.");
-
-            while(!a.equals(password)) {
-                a= input.nextLine();
-
-                if (a.equals("hairyharry") ) {
-                    System.out.println("Passwordet du indtastede er korrekt, du vil nu blive logget ind.");
-                }
-                else if (a.equals("q")) {
-                    break;
-                } else {
-                    System.out.println("Passwordet du indtastede var forket, prøv igen");
-                }
+    static boolean password(){
 
 
+        Scanner input=new Scanner(System.in);
+        passwords="hairyharry";
+        a="";
+
+        System.out.println("Indtast dit Password");
+        System.out.println("Tast q for at afslutte.");
+
+        while(!a.equals(passwords)) {
+            a= input.nextLine();
+
+            if (a.equals("hairyharry") ) {
+                System.out.println("Passwordet du indtastede er korrekt, du vil nu blive logget ind.");
+                return true;
+            }
+            else if (a.equals("q")) {
+                System.out.println("Du vil nu blive sendt tilbage.");
+                return false;
+            } else {
+                System.out.println("Passwordet du indtastede var forket, prøv igen");
             }
 
-        }
-    }
 
+        }
+        return false;
+    }
+}
