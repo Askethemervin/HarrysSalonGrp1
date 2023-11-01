da3a61db730c05612ecde1c8663a788709c8cea2
bjli0001 <bjli0001@stud.kea.dk>
2023-11-01 10:32:55 +0100

    Lidt oprydning.

--- src/Calender.java
+++ src/Calender.java
@@ -4,47 +4,28 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 
 public class Calender {
     static SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
 
-    public static void main(String[] args) throws IOException {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-        Calendar c = Calendar.getInstance();
-        c.setTime(new Date());
-
-        List<String[]> dataLines = new ArrayList<>();
-
-
-        for (int i=0; i<365; i++){
-
-               switch (c.get(Calendar.DAY_OF_WEEK)) {
-                   case 2 -> dataLines.add(new String[]{sdf.format(c.getTime()), "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0","Mandag"});
-                   case 3 -> dataLines.add(new String[]{sdf.format(c.getTime()), "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0","Tirsdag"});
-                   case 4 -> dataLines.add(new String[]{sdf.format(c.getTime()), "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0","Onsdag"});
-                   case 5 -> dataLines.add(new String[]{sdf.format(c.getTime()), "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0","Torsdag"});
-                   case 6 -> dataLines.add(new String[]{sdf.format(c.getTime()), "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0","Fredag"});
-               }
-
-            c.add(Calendar.DATE, 1);
-
-        }
-
-        ToFile.saveList(dataLines,"calender.txt");
-    }
-
-
 
     public static void addDays(ArrayList<String> dates) throws ParseException, IOException {
-        String dateString = dates.get(dates.size()-1);
-        Date date = sdf.parse(dateString);
-        long diff = TimeUnit.DAYS.convert(date.getTime()-(new Date()).getTime(), TimeUnit.MILLISECONDS);
+        long diff;
         Calendar d = Calendar.getInstance();
-        d.setTime(date);
+        if (dates.isEmpty()){
+            d.setTime(new Date());
+            d.add(Calendar.DATE, -1);
+            diff=0;
 
+        }
+        else {
+            String dateString = dates.get(dates.size() - 1);
+            Date date = sdf.parse(dateString);
+            diff = TimeUnit.DAYS.convert(date.getTime() - (new Date()).getTime(), TimeUnit.MILLISECONDS);
+            d.setTime(date);
+        }
         for (int i=0; i<=365-diff; i++){
             d.add(Calendar.DATE, 1);
             switch (d.get(Calendar.DAY_OF_WEEK)) {
--- src/ToFile.java
+++ src/ToFile.java
@@ -2,12 +2,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class ToFile {
 
-    public static void saveList(List<String[]> dataLines,String s) throws IOException {
+    public static void saveList(ArrayList<String[]> dataLines,String s) throws IOException {
         FileWriter fil = new FileWriter(s);
         PrintWriter ud = new PrintWriter(fil);
         for (String[] sa: dataLines){
@@ -15,7 +14,7 @@ public class ToFile {
         }
         fil.close();
     }
-    public static void saveCustomer(List<Customer> dataLines) throws IOException {
+    public static void saveCustomer(ArrayList<Customer> dataLines) throws IOException {
         FileWriter fil = new FileWriter("customers.txt");
         PrintWriter ud = new PrintWriter(fil);
 
