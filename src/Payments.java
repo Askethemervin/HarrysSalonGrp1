2c79eb1b0e5b3a79242f9352f5720754eec2ddcf
bjli0001 <bjli0001@stud.kea.dk>
2023-11-01 13:49:07 +0100

    .

--- src/Payments.java
+++ src/Payments.java
@@ -3,20 +3,23 @@ public class Payments {
         System.out.println("Kunder og reservationer for dato: " + date);
         boolean found = false;
         int totalPrice = 0;
+        int dateIndex = Main.dates.indexOf(date);
+        String telnr ;
+        int custIndex;
 
-        for (Customer customer : Main.customers) {
-            for (String[] booking : customer.bookings) {
-                if (booking[0].equals(date)) {
-                    System.out.println("Kunde: " + customer.name);
-                    System.out.println("Telefon nr.: " + customer.tlfnr);
-                    System.out.println("Tidspunkt: " + booking[1]);
-                    if (Integer.parseInt(Book.payedPrice(date,booking[1]))<0){
-                        System.out.println("Aftale om kredit: "+Integer.parseInt(Book.payedPrice(date,booking[1]))*-1);
-                    }
-                    else {System.out.println("Transaktion: " + Book.payedPrice(date, booking[1])+"\n");
-                    }
-
-                    totalPrice = totalPrice+Integer.parseInt(Book.payedPrice(date, booking[1]));
+        for (int i=1; i<17; i++){
+            telnr=Main.calender.get(dateIndex)[i];
+            if (!telnr.equals("0") && !telnr.equals("1")) {
+                custIndex = Main.phoneNumbers.indexOf(telnr);
+                System.out.println("Kunde: " + Main.customers.get(custIndex).name);
+                System.out.println("Telefon nr.: " + Main.customers.get(custIndex).tlfnr);
+                System.out.println("Tidspunkt: " + Available.timeindex(i));
+                if (Integer.parseInt(Book.payedPrice(date,Available.timeindex(i)))<0){
+                    System.out.println("Aftale om kredit: "+Integer.parseInt(Book.payedPrice(date,Available.timeindex(i)))*-1);
+                }
+                else {
+                    System.out.println("Transaktion: " + Book.payedPrice(date, Available.timeindex(i))+"\n");
+                    totalPrice = totalPrice+Integer.parseInt(Book.payedPrice(date, Available.timeindex(i)));
 
                     found = true;
                 }
--- src/ToFile.java
+++ src/ToFile.java
@@ -3,13 +3,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
-
 public class ToFile {
 
     public static void saveList(ArrayList<String[]> dataLines,String s) throws IOException {
         FileWriter fil = new FileWriter(s);
         PrintWriter ud = new PrintWriter(fil);
+
         for (String[] sa: dataLines){
+
             ud.println(String.join(",",sa));
         }
         fil.close();
@@ -22,11 +23,9 @@ public class ToFile {
             ArrayList<String> bookArr = new ArrayList<>();
             for (String[] s: k.bookings){
 
-
                 bookArr.add(String.join(".",s));
 
             }
-
             ud.println(k.tlfnr +","+ k.name +","+ String.join(";",bookArr));
         }
         fil.close();
